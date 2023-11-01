7ec72261f9195339458c092db57d773a7380ed89
Mike <mik3waser@gmail.com>
2023-11-01 04:18:04 +0100

    html and css

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+/*
 fetch('https://majazocom.github.io/Data/solaris.json')
     .then(res => res.json())
     .then (data => console.table(data))
     .catch(error => console.log("ERROR"))
+*/
+
+let solarisData;
+
+fetch('https://majazocom.github.io/Data/solaris.json')
+  .then(response => response.json())
+  .then(data => {
+    solarisData = data;
+
+    console.table(solarisData);
+
+    solarisData.forEach(planet => {
+      console.log(`Planet namn: ${planet.name}`);
+    });
+  })
+  .catch(error => {
+    console.error('Något gick fel:', error);
+  });
